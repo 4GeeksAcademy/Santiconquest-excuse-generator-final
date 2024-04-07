8ae298703e7b8a105cc0551fdd0786f1165dfe84
Santiconquest <161180110+Santiconquest@users.noreply.github.com>
2024-04-07 22:05:49 +0000

    Entrego proyecto de generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The driver", "The teacher", "The gardener", "The pet"];
+  let action = ["insult", "hit", "attack", "bite"];
+  let what = ["the policeman", "the banner", "a tree", "my car"];
+  let when = [
+    "during the lunch",
+    "this morning",
+    "when i was at the gym",
+    "before the class",
+    "this evening"
+  ];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  const generator = `Could you believe that.. ${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
+
+  const titleElement = document.querySelector("#excuse");
+  titleElement.innerHTML = generator;
+  console.log(titleElement);
 };
